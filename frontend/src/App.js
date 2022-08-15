b8a9bfde14bb05d0a503fa95a9fc710a59b3e27c
ferbz <mikerudyharris@gmail.com>
2022-08-15 19:24:02 +0200

    Repo to github

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Navbar from "./components/NavBar";
+import Register from "./components/Register";
+import React from "react";
+ 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/dashboard" element={
+            <>
+              <Navbar/>
+              <Dashboard/>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+ 
+export default App;
